f4f1af97b5da427ca3f9dce3a35b08d60131c373
mahmouddsalama <mahmoud.salama.gado@gmail.com>
2025-10-21 21:15:06 +0300

    packages

--- src/main/java/com/example/demo/rest/DeomRestController.java
+++ src/main/java/com/example/demo/rest/DeomRestController.java
@@ -1,8 +1,10 @@
-package com.example.demo;
+package com.example.demo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.common.Coach;
 
 @RestController
 public class DeomRestController {
